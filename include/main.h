7e216292f6ae0de1cf98b49b1f95efa7ff0a5774
Noeël Moeskops <noeel.moeskops@hva.nl>
2018-12-09 01:45:43 +0100

    gitignore en bestanden aangemaakt

---
+++ include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by noeel on 9-12-18.
+//
+
+#ifndef ARUNA_MAIN_H
+#define ARUNA_MAIN_H
+
+void main_app();
+
+#endif //ARUNA_MAIN_H
---
+++ src/main.c
@@ -0,0 +1,9 @@
+//
+// Created by noeel on 9-12-18.
+//
+
+#include "main.h"
+
+void app_main() {
+
+}
