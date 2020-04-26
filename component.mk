47ac61c5a9c2adfe1c3111f5e7f8f86e987e1c71
Noeël <noeelmoeskops@hotmail.com>
2020-04-26 15:32:44 +0200

    make build fix

--- component.mk
+++ component.mk
@@ -2,5 +2,5 @@
 # Main component makefile.
 #
 
-COMPONENT_SRCDIRS := src src/comm src/control src/blinky src/log src/comm/portable/esp32 src/control/portable/esp32
+COMPONENT_SRCDIRS := src src/comm src/control src/blinky src/log src/comm/portable/esp32 src/control/portable/esp32 src/sis src/sis/portable/esp32
 COMPONENT_ADD_INCLUDEDIRS = include
