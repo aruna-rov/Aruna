5f38fa998691dd187798f99de1f6e764abdb7b45
Noeël Moeskops <noeelmoeskops@Hotmail.com>
2020-10-22 20:57:35 +0200

    removed old PCA9685 driver

--- include/aruna.h
+++ include/aruna.h
@@ -27,7 +27,6 @@
     #include "aruna/sis/portable/esp32/WaterSensor.h"
     #include "aruna/driver/ADS101x/ADS101x.h"
     #include "aruna/sis/ADS101xWaterSensor.h"
-    #include "aruna/control/PCA9685.h"
 
 #elif defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
     // System is Posix
