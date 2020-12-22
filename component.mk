5233d5d09f188b55ecc2e404d801646187903693
Noeël Moeskops <noeelmoeskops@Hotmail.com>
2020-12-22 23:25:07 +0100

    removed redundant Makefile

--- component.mk
+++
@@ -1,7 +0,0 @@
-#
-# Main component makefile.
-#
-
-COMPONENT_SRCDIRS := src src/comm src/control src/log src/comm/portable/esp32 src/control/portable/esp32 src/sis src/sis/portable/esp32
-COMPONENT_ADD_INCLUDEDIRS = include
-COMPONENT_REQUIRES = esp_adc_cal
